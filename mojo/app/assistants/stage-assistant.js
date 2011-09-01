94f5eaceca6ec35c095fd80aec75b8acfd7b1fdb
Florian Jenn <devel@effjot.net>
2011-09-01 22:11:15 +0200

    Displaying in various units (mm, inch, pixel) possible: width and height now stored as numbers, not strings.  Prefs contain relevant entries (unit, dpi).  Conversion function.

--- mojo/app/assistants/stage-assistant.js
+++ mojo/app/assistants/stage-assistant.js
@@ -28,66 +28,66 @@ function StageAssistant() {
         seriesItems: {
             P: {                // portrait ("L" for landscape will be filled in below)
                 A: [
-                    { dt: "A0",  width: "841", height: "1189" },
-                    { dt: "A1",  width: "594", height: "841" },
-                    { dt: "A2",  width: "420", height: "594" },
-                    { dt: "A3",  width: "297", height: "420" },
-                    { dt: "A4",  width: "210", height: "297" },
-                    { dt: "A5",  width: "148", height: "210" },
-                    { dt: "A6",  width: "105", height: "148" },
-                    { dt: "A7",  width:  "74", height: "105" },
-                    { dt: "A8",  width:  "52", height:  "74" },
-                    { dt: "A9",  width:  "37", height:  "52" },
-                    { dt: "A10", width:  "26", height:  "37" }
+                    { dt: "A0",  width: 841, height: 1189 },
+                    { dt: "A1",  width: 594, height: 841 },
+                    { dt: "A2",  width: 420, height: 594 },
+                    { dt: "A3",  width: 297, height: 420 },
+                    { dt: "A4",  width: 210, height: 297 },
+                    { dt: "A5",  width: 148, height: 210 },
+                    { dt: "A6",  width: 105, height: 148 },
+                    { dt: "A7",  width:  74, height: 105 },
+                    { dt: "A8",  width:  52, height:  74 },
+                    { dt: "A9",  width:  37, height:  52 },
+                    { dt: "A10", width:  26, height:  37 }
                 ],
 
                 B: [
-                    { dt: "B0",  width: "1000", height: "1414" },
-                    { dt: "B1",  width: "707", height: "1000" },
-                    { dt: "B2",  width: "500", height: "707" },
-                    { dt: "B3",  width: "353", height: "500" },
-                    { dt: "B4",  width: "250", height: "353" },
-                    { dt: "B5",  width: "176", height: "250" },
-                    { dt: "B6",  width: "125", height: "176" },
-                    { dt: "B7",  width:  "88", height: "125" },
-                    { dt: "B8",  width:  "62", height:  "88" },
-                    { dt: "B9",  width:  "44", height:  "62" },
-                    { dt: "B10", width:  "31", height:  "44" }
+                    { dt: "B0",  width: 1000, height: 1414 },
+                    { dt: "B1",  width: 707, height: 1000 },
+                    { dt: "B2",  width: 500, height: 707 },
+                    { dt: "B3",  width: 353, height: 500 },
+                    { dt: "B4",  width: 250, height: 353 },
+                    { dt: "B5",  width: 176, height: 250 },
+                    { dt: "B6",  width: 125, height: 176 },
+                    { dt: "B7",  width:  88, height: 125 },
+                    { dt: "B8",  width:  62, height:  88 },
+                    { dt: "B9",  width:  44, height:  62 },
+                    { dt: "B10", width:  31, height:  44 }
                 ],
 
                 C: [
-                    { dt: "C0",  width: "917", height: "1297" },
-                    { dt: "C1",  width: "648", height: "917" },
-                    { dt: "C2",  width: "458", height: "648" },
-                    { dt: "C3",  width: "324", height: "458" },
-                    { dt: "C4",  width: "229", height: "324" },
-                    { dt: "C5",  width: "162", height: "229" },
-                    { dt: "C6",  width: "114", height: "162" },
-                    { dt: "C7",  width:  "81", height: "114" },
-                    { dt: "C8",  width:  "57", height:  "81" },
-                    { dt: "C9",  width:  "40", height:  "57" },
-                    { dt: "C10", width:  "28", height:  "40" }
+                    { dt: "C0",  width: 917, height: 1297 },
+                    { dt: "C1",  width: 648, height: 917 },
+                    { dt: "C2",  width: 458, height: 648 },
+                    { dt: "C3",  width: 324, height: 458 },
+                    { dt: "C4",  width: 229, height: 324 },
+                    { dt: "C5",  width: 162, height: 229 },
+                    { dt: "C6",  width: 114, height: 162 },
+                    { dt: "C7",  width:  81, height: 114 },
+                    { dt: "C8",  width:  57, height:  81 },
+                    { dt: "C9",  width:  40, height:  57 },
+                    { dt: "C10", width:  28, height:  40 }
                 ],
 
                 D: [
-                    { dt: "D0",  width: "771", height: "1091" },
-                    { dt: "D1",  width: "545", height: "771" },
-                    { dt: "D2",  width: "385", height: "545" },
-                    { dt: "D3",  width: "272", height: "385" },
-                    { dt: "D4",  width: "192", height: "272" },
-                    { dt: "D5",  width: "136", height: "192" },
-                    { dt: "D6",  width:  "96", height: "136" },
-                    { dt: "D7",  width:  "68", height:  "96" }
+                    { dt: "D0",  width: 771, height: 1091 },
+                    { dt: "D1",  width: 545, height: 771 },
+                    { dt: "D2",  width: 385, height: 545 },
+                    { dt: "D3",  width: 272, height: 385 },
+                    { dt: "D4",  width: 192, height: 272 },
+                    { dt: "D5",  width: 136, height: 192 },
+                    { dt: "D6",  width:  96, height: 136 },
+                    { dt: "D7",  width:  68, height:  96 }
                 ],
 
                 N: [
-                    { dt: "Broadsheet",   width: "432", height: "559" },     // 241488 mm²
-                    { dt: "Ledger / Tabloid", width: "279", height: "432" }, // 120528 mm²
-                    { dt: "Legal",        width: "216", height: "356" },     //  76896 mm²
-                    { dt: "Letter",       width: "216", height: "279" },     //  60264
-                    { dt: "Executive",    width: "184", height: "267" },     //  49128
-                    { dt: "Invoice",      width: "140", height: "216" },     //  30240
-                    { dt: "Junior Legal", width: "127", height: "203" }      //  25781
+                    { dt: "Broadsheet",   width: 432, height: 559 },     // 241488 mm²
+                    { dt: "Ledger / Tabloid", width: 279, height: 432 }, // 120528 mm²
+                    { dt: "Legal",        width: 216, height: 356 },     //  76896 mm²
+                    { dt: "Letter",       width: 216, height: 279 },     //  60264
+                    { dt: "Executive",    width: 184, height: 267 },     //  49128
+                    { dt: "Invoice",      width: 140, height: 216 },     //  30240
+                    { dt: "Junior Legal", width: 127, height: 203 }      //  25781
                 ]
             }
         }
@@ -119,7 +119,9 @@ function StageAssistant() {
 
     Papersizes.prefs = {
         startseries: "A",
-        keeplast:    true // not yet used; last series always kept
+        keeplast:    true, // not yet used; last series always kept
+        unit:        "px",
+        dpi:         300
     };
 
     // app menu
@@ -131,6 +133,17 @@ function StageAssistant() {
             {label: $L("About"), command: 'do-about' }
         ]
     };
+
+    // convert between units
+
+    Papersizes.toUnit = function(x, unit) {
+        switch (unit) {
+        case "mm": return x;
+        case "in": return (x / 25.4).toFixed(1);
+        case "px": return Math.round(x / 25.4 * Papersizes.prefs.dpi);
+        default: Mojo.Controller.errorDialog("Invalid unit in conversion.");
+        }
+    }
 };
 
 
@@ -143,7 +156,9 @@ StageAssistant.prototype.setup = function() {
     this.cookie = new Mojo.Model.Cookie("PapersizesPrefs");
     var cookiedata = this.cookie.get();
     if (cookiedata) {
-        Papersizes.prefs = { startseries: cookiedata.startseries };
+        Papersizes.prefs = { startseries: cookiedata.startseries,
+                             unit:        cookiedata.unit,
+                             dpi:         cookiedata.dpi };
     } else {
         this.cookie.put(Papersizes.prefs);
     }
@@ -182,3 +197,4 @@ StageAssistant.prototype.handleCommand = function(event) {
         }
     }
 };
+
